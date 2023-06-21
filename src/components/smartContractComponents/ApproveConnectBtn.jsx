e780cb6495d4f075c8ebdf301fc818fe36657a95
Marina2022 <marusiiiia@yandex.ru>
2023-06-21 15:57:35 +0300

    added wait... on button

--- src/components/smartContractComponents/ApproveConnectBtn.jsx
+++ src/components/smartContractComponents/ApproveConnectBtn.jsx
@@ -1,11 +1,21 @@
 import s from './ConnectBtns.module.scss';
+import {useState} from "react";
 
 const ApproveConnectBtn = ({onVerifyClick}) => {
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const onBtnClick = ()=>{
+    setIsSubmitting(true)
+    onVerifyClick()
+  }
+
   return (
     <button
       className={s.connectBtn}
-      onClick={onVerifyClick}  // тут будет онКлик кнопки Register
-    >Verify</button>
+      onClick={onBtnClick}  // тут будет онКлик кнопки Register
+      disabled={isSubmitting}
+    >{isSubmitting ? 'Wait...' : 'Verify'}</button>
   );
 };
 
